c9f9d249940b95d3cfd9f64c8339dadfaa707435
Cagri Yolyapar <cagri.yolyapar@gmail.com>
2022-02-15 14:12:01 +0300

    Add project files.

---
+++ MVCArea_1/Areas/Yazilim/YazilimAreaRegistration.cs
@@ -0,0 +1,24 @@
+﻿using System.Web.Mvc;
+
+namespace MVCArea_1.Areas.Yazilim
+{
+    public class YazilimAreaRegistration : AreaRegistration 
+    {
+        public override string AreaName 
+        {
+            get 
+            {
+                return "Yazilim";
+            }
+        }
+
+        public override void RegisterArea(AreaRegistrationContext context) 
+        {
+            context.MapRoute(
+                "Yazilim_default",
+                "Yazilim/{controller}/{action}/{id}",
+                new { action = "Index", id = UrlParameter.Optional }
+            );
+        }
+    }
+}
